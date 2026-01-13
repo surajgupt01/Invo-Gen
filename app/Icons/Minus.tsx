325d6fa978c4b5230d1b244bd32432c29d0b9f4d
surajgupt01 <surajgupt880@gmail.com>
2026-01-13 15:54:40 +0530

    v-3

---
+++ app/Icons/Minus.tsx
@@ -0,0 +1,14 @@
+export default function Minus() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="size-4"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M5 12h14" />
+    </svg>
+  );
+}
