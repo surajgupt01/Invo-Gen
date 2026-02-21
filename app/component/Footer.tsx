3983935e28390d547cb698f2a24ab924eafd69a5
surajgupt01 <surajgupt880@gmail.com>
2026-02-21 23:16:38 +0530

    logo changes

--- app/component/Footer.tsx
+++ app/component/Footer.tsx
@@ -1,21 +1,24 @@
+import Image from "next/image";
+
 export default function Footer() {
   return (
-    <div className=" w-full bg-black min-h-100 text-gray-500 text-sm p-0">
-      <div className="  flex sm:flex-row flex-col justify-between font-medium tracking-wide">
+    <div className=" w-full  min-h-100 border-t border-gray-100 text-gray-800 text-sm p-0">
+      <div className="  flex sm:flex-row flex-col justify-between items-center font-medium tracking-wide">
         <div className="mt-20 sm:ml-10  py-4 px-2 sm:w-[55%] w-full ">
           <div className="text-4xl  flex gap-1 items-center">
             <div className="">{/* <Logo textColor="text-white"></Logo> */}</div>
-            <span className="text-white">
-              Ledg
+
+            <span className="text-gray-800">
+              Lu
               <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-blue-400">
-                erly
+                en
               </span>
             </span>
           </div>
           {/* <p className="ml-2 flex">Professional Invoices</p> */}
 
-          <div className="sm:text-lg text-md  mt-4 tracking-widest px-2">
-            <p className="text-gray-400">
+          <div className="sm:text-lg text-md mt-2 tracking-widest px-2">
+            <p className="text-gray-800">
               Create professional invoices in seconds — without spreadsheets.
             </p>
             <p className="sm:text-sm text-sm mt-2">
@@ -24,10 +27,10 @@ export default function Footer() {
             </p>
 
             <div className="text-xs mt-6 flex items-center gap-2 ">
-              <button className="rounded-xs hover:shadow-lg shadow-gray-900 hover:-translate-y-1 duration-300 cursor-pointer hover:text-teal-500 font-normal border-1 p-2 bg-white text-black">
+              <button className="rounded-xs hover:shadow-lg shadow-gray-100 hover:border-gray-400 hover:-translate-y-1 duration-300 cursor-pointer hover:text-teal-500 font-normal border-1 p-2 bg-white text-black">
                 Contact Sales
               </button>
-              <button className="rounded-xs hover:shadow-lg shadow-gray-900 hover:-translate-y-1 duration-500 cursor-pointer hover:text-teal-500 font-normal border-1 p-2 bg-black text-white">
+              <button className="rounded-xs hover:shadow-lg shadow-gray-200 hover:-translate-y-1 duration-300 cursor-pointer hover:text-teal-500 font-normal border-1 p-2 bg-black text-white">
                 Try now
               </button>
             </div>
@@ -42,23 +45,23 @@ export default function Footer() {
         <div className="flex flex-col items-center sm:w-[60%] w-full mt-20">
           <div className="flex  justify-evenly w-full text-xs">
             <div className="flex flex-col gap-4 items-start">
-              <p className="cursor-pointer hover:text-gray-300">About</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">Features</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">Pricing</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">Contact</p>{" "}
-              <p className="cursor-pointer hover:text-gray-300">Blog</p>{" "}
+              <p className="cursor-pointer hover:text-gray-500">About</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">Features</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">Pricing</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">Contact</p>{" "}
+              <p className="cursor-pointer hover:text-gray-500">Blog</p>{" "}
             </div>
             <div className="flex flex-col gap-4 items-start">
-              <p className="cursor-pointer hover:text-gray-300 ">
+              <p className="cursor-pointer hover:text-gray-500 ">
                 Documentation
               </p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">FAQ</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">Support</p>
+              <p className="cursor-pointer  hover:text-gray-500">FAQ</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">Support</p>
             </div>
             <div className="flex flex-col gap-4 items-start">
-              <p className="cursor-pointer  hover:text-gray-300">X(Twitter)</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">LinkedIn</p>{" "}
-              <p className="cursor-pointer  hover:text-gray-300">YouTube</p>
+              <p className="cursor-pointer  hover:text-gray-500">X(Twitter)</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">LinkedIn</p>{" "}
+              <p className="cursor-pointer  hover:text-gray-500">YouTube</p>
             </div>
           </div>
         </div>
@@ -67,11 +70,11 @@ export default function Footer() {
       <div
         className="w-full  flex justify-center items-center p-4 mt-20 cursor-pointer 
               xl:text-[200px]  lg:text-[150px] sm:text-[100px] md:text-[120px] text-[80px] font-bold tracking-widest
-                bg-gradient-to-b from-gray-800 via-gray-800 to-black
-                bg-clip-text text-transparent 
-                drop-shadow-[0_5px_15px_rgba(0,0,0,0.15)]"
+                bg-gradient-to-b from-black via-gray-300 to-white
+                bg-clip-text text-transparent text-shadow-xl text-shadow-gray-700
+                "
       >
-        Ledgerly
+        {"Luen"}
       </div>
     </div>
   );
--- app/component/Nav.tsx
+++ app/component/Nav.tsx
@@ -1,32 +1,42 @@
 import Link from "next/link";
 import Logo from "../Icons/Logo";
+import Image from "next/image";
 
-
-interface NavProp{
-  textColor : string
+interface NavProp {
+  textColor: string;
 }
-export function NavLogo({textColor} : NavProp) {
+export function NavLogo({ textColor }: NavProp) {
   return (
-    <div className="  w-full cursor-pointer font-bold sm:text-lg text-xs flex items-center ">
-      <Logo textColor = {textColor}/>
+    <div className="  w-full cursor-pointer font-bold sm:text-xl tracking-widest text-xs flex items-center ">
+      {/* <Logo textColor = {textColor}/> */}
       <span className={`${textColor}`}>
-        Ledg
-        <span className="text-transparent bg-clip-text bg-gradient-to-r from-green-500 to-blue-400">
-          erly
+        Lu
+        <span className="text-transparent bg-clip-text tracking-widest  bg-gradient-to-r from-green-500 to-blue-400">
+          en
         </span>{" "}
       </span>
+      {/* <Image
+        alt="logo"
+        src={"/ChatGPT Image Feb 21, 2026, 03_16_14 PM.png"}
+        width={200}
+        height={200}
+      ></Image> */}
     </div>
   );
 }
 
 export default function Nav() {
   return (
-    <div className="flex justify-evenly  w-full  p-1 px-4   fixed       bg-gray-300/10 backdrop-blur-sm  z-100">
-      <NavLogo textColor = "text-black"/>
+    <div className="flex justify-evenly  w-full  p-1 px-4   fixed h-15      bg-gray-300/10 backdrop-blur-sm  z-100">
+      <NavLogo textColor="text-black" />
       <div className="flex p-2   w-full justify-end items-center">
         {/* <li className="cursor-pointer hover:text-gray-700">Home</li> */}
-        <Link href={'#PriceSection'} className="md:text-xs text-xs sm:mr-4 mr-2 hover:bg-gray-200 p-2 rounded-full cursor-pointer text-shadow-xs tracking-wide ease-in-out duration-500 font-semibold flex justify-center items-center">
-          {`go`}<span className="text-teal-400 ">pro</span>
+        <Link
+          href={"#PriceSection"}
+          className="md:text-xs text-xs sm:mr-4 mr-2 hover:bg-gray-200 p-2 rounded-full cursor-pointer text-shadow-xs tracking-wide ease-in-out duration-500 font-semibold flex justify-center items-center"
+        >
+          {`go`}
+          <span className="text-teal-400 ">pro</span>
         </Link>
         <div>
           <Link
