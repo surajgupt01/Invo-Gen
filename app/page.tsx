60da4996284184e163c1ade0e460b01494b41a28
surajgupt01 <surajgupt880@gmail.com>
2026-02-22 21:28:15 +0530

    Landing page desgin changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,32 +5,37 @@ import HeroSection from "./component/HeroSection";
 import Nav from "./component/Nav";
 import Tick from "./Icons/Tick";
 import Link from "next/link";
+import VideoIcon from "./Icons/Video";
+import { Descriptions } from "./Elements";
+import Image from "next/image";
+import Template from "./Icons/Tempelate";
+import Docs from "./Icons/Doc";
 
 export default function Home() {
   return (
     <div className="w-full h-full selection:bg-teal-400 scroll-smooth tracking-wide ">
-      <div className=" bg-linear-to-b from-gray-200/20 via-teal-50 to-gray-200/20 flex flex-col items-center scroll-smooth  ">
+      <div className="  flex flex-col items-center scroll-smooth  ">
         <Nav />
 
         <HeroSection />
 
-<div
-  className="
+        <div
+          className="
     inline-flex justify-center items-start
-    rounded-xl md:p-3 p-2 m-2
+    rounded-3xl md:p-3 p-2 m-2
     sm:mt-6 mt-10
-    bg-teal-400/40
-    shadow-lg shadow-emerald-400/30
+    bg-gradient-to-b from bg-teal-500/60 via-green-400/80  to-blue-500/60
+    shadow-xl shadow-emerald-400/30 hover:shadow-2xl duration-300 ease-in-out cursor-pointer
   "
->
-  <img
-    src="/dash-Img.png"
-    className="md:w-[65vw] w-[97vw] md:h-auto h-[26vh] rounded-lg"
-    alt="dashboard"
-  />
-</div>
-
-
+        >
+          <Image
+            width={2000}
+            height={4000}
+            src="/dash-Img.png"
+            className="md:w-[65vw] w-[97vw] md:h-auto h-[26vh] rounded-2xl"
+            alt="dashboard"
+          />
+        </div>
 
         <div className="md:flex flex md:flex-row flex-col  justify-center items-center gap-6 md:mt-20 mt-5  p-8 h-150 w-full ">
           <div className="font-bold flex-col md:text-4xl sm:text-3xl text-xl md:w-[80%] w-full  flex items-center justify-center md:mt-30 mt-15 p-6 text-gray-600">
@@ -80,26 +85,141 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="md:flex md:flex-row flex flex-col justify-center items-center gap-6 mt-20   md:p-8 p-4 md:h-100 h-auto bg-teal-100 w-full  ">
-          <div className="font-bold  flex-col md:text-4xl sm:text-3xl text-xl flex items-start justify-center md:mt-30 mt-0 md:p-6 p-2 text-gray-600">
-            <p className=" md:text-left text-center leading-12 font-medium  ">
-              <span className="md:text-6xl text-5xl">Unlock</span> Your Business
-              Potential <span className="md:text-5xl text-3xl">Today!</span>
+        <div className="md:flex md:flex-row flex flex-col mt-20  lg:px-35 px-5  h-auto bg-gradient-to-b from-neutral-950 via-gray-950 to-neutral-950 w-full">
+          <div className="font-bold border-l border-gray-900 w-full  flex-col md:text-4xl sm:text-3xl text-xl flex items-start justify-center  mt-0  py-6 text-teal-100">
+            <p className=" md:text-left text-center leading-12 font-medium  px-2">
+              Unlock Your Business Potential Today!
             </p>
 
-            <p className="  md:mt-6 mt-4 font-light sm:text-lg text-sm ml-2">
+            <p className="mt-4 font-light sm:text-lg text-sm ml-2 text-teal-100 px-2">
               No complexity—just clean, professional invoices.
             </p>
-            <Link href={"/signin"}>
-              <div className="md:p-3 p-2 flex justify-center items-center bg-black text-white md:text-sm md:mt-4 mt-2 text-sm  rounded-3xl shadow-md shadow-gray-400 cursor-pointer hover:scale-105 transition-all ease-in-out duration-500">
-                Get Started
+            <div className="flex items-center border-b border-gray-900 w-full p-2">
+              <Link href={"/signin"}>
+                <div
+                  className="md:py-3 py-2 px-6 ml-2 flex justify-center items-center active:scale-90  bg-neutral-50 tracking-wide   
+                rounded-xs text-black md:text-sm md:mt-4 mt-2 text-sm cursor-pointer transition-all ease-in-out duration-500"
+                >
+                  Get Started
+                </div>
+              </Link>
+              <div
+                className="md:py-3 py-2 px-6 ml-2 flex justify-center gap-1 border-teal-950 hover:border-teal-900 border items-center active:scale-90  bg-neutral-950 tracking-wide   
+                rounded-xs text-white md:text-sm md:mt-4 mt-2 text-sm cursor-pointer transition-all ease-in-out duration-500"
+              >
+                see how it works
+                <VideoIcon/>
               </div>
-            </Link>
+            </div>
+
+            <div
+              className=" rounded-xs p-4 w-full h-auto mt-4"
+
+              //             bg-[radial-gradient(#0d9488_0.5px,transparent_0.5px)]
+              // [background-size:10px_10px]
+            >
+              <section className="py-4">
+                <div className="max-w-6xl mx-auto">
+                  <div className="">
+                    <h2 className="text-2xl font-bold text-gray-200">
+                      Everything You Need to Invoice Professionally
+                    </h2>
+                    <p className="text-gray-400 font-light text-sm max-w-2xl">
+                      Powerful features designed to simplify your billing
+                      workflow.
+                    </p>
+                  </div>
+                </div>
+              </section>
+
+              <div className="w-full h-auto   flex justify-center items-center">
+                <div className="grid grid-cols-3 gap-2 lg:w-[50%] w-full">
+                  <div className="col-span-2 bg-teal-800 h-50  flex-col">
+                    <div
+                      className="w-full h-full  bg-white flex-col gap-2 flex justify-center items-start p-4 text-gray-700 text-[16px]  
+                       border border-white/20 transition duration-300 
+                     "
+                    >
+                      <span className="text-black text-2xl">
+                        {Descriptions[0].title}
+                      </span>
+                      <span className="text-[8px] font-light text-left">
+                        {Descriptions[0].desc}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="col-span-1 bg-indigo-800 h-50 ">
+                    <div
+                      className="w-full h-full  flex-col gap-2 flex justify-center items-start lg:p-4 p-1 text-gray-700 text-[16px]  
+                       transition duration-300 
+                     "
+                    >
+                      <span className="text-indigo-300 lg:text-2xl text-xl">
+                        {Descriptions[1].title}
+                      </span>
+                      <span className="lg:text-[10px] text-[8px] text-indigo-400  font-light text-left">
+                        {Descriptions[1].desc}
+                      </span>
+                    </div>
+                  </div>
+                  <div className="col-span-3 row-span-3 bg-emerald-950 h-60 w-auto">
+                    <div className="grid md:grid-cols-2 gap-8 p-2 h-full">
+                      {/* LEFT SIDE - Text */}
+                      <div className="flex-col flex justify-center items-center">
+                        <h3 className="text-green-100 text-2xl">
+                          Smart Dashboard Insights
+                        </h3>
+                        <p className="text-emerald-200/80 text-[10px] mt-4 font-normal">
+                          Track revenue, monitor payment status, and gain
+                          real-time clarity.
+                        </p>
+                      </div>
+
+                      {/* RIGHT SIDE - Graphs */}
+                      <div className="bg-black/30 rounded-xs p-6 backdrop-blur-xl">
+                        {/* Your charts here */}
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-span-1  bg-blue-800 h-40 ">
+                    <div
+                      className="w-full h-full  flex-col gap-2 flex justify-center items-start lg:p-4 p-1 text-gray-700 text-[16px]  
+                       transition duration-300 
+                     "
+                    >
+                      <span className="text-white lg:text-2xl text-lg">
+                        {Descriptions[2].title}
+                      </span>
+                      <span className="lg:text-[10px] text-[7px] text-gray-300 font-light text-left">
+                        {Descriptions[2].desc}
+                      </span>
+                    </div>
+                  </div>
+
+                  <div className="col-span-2 row-span-3 bg-sky-950 h-40 w-auto">
+                    <div className="col-span-1  bg-fuchsia-800 h-40 ">
+                      <div
+                        className="w-full h-full  flex-col gap-2 flex justify-center items-start p-4 text-gray-700 text-[16px]  
+                       transition duration-300 
+                     "
+                      >
+                        <span className="text-fuchsia-200 text-2xl">
+                          {Descriptions[3].title}
+                        </span>
+                        <span className="text-[10px] text-fuchsia-300 font-light text-left">
+                          {Descriptions[3].desc}
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
           {/* <div className="w-[2px] bg-gray-300 h-full"></div> */}
-          <div className="md:w-auto w-full  h-auto  ">
+          {/* <div className="md:w-auto w-full  h-auto  ">
             <Features />
-          </div>
+          </div> */}
         </div>
 
         <FAQ />
