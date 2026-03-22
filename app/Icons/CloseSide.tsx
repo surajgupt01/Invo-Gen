7af0807705341e3db6b8cf8a0856336fa5a796a9
surajgupt01 <surajgupt880@gmail.com>
2026-03-23 02:16:51 +0530

    Free-Tier

---
+++ app/Icons/CloseSide.tsx
@@ -0,0 +1,21 @@
+export default function CloseSide() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      viewBox="0 0 24 24"
+      fill="currentColor"
+      className="size-6 text-neutral-800 hover:text-neutral-700 duration-300 ease-in-out cursor-pointer"
+    >
+      <path
+        fillRule="evenodd"
+        d="M10.72 11.47a.75.75 0 0 0 0 1.06l7.5 7.5a.75.75 0 1 0 1.06-1.06L12.31 12l6.97-6.97a.75.75 0 0 0-1.06-1.06l-7.5 7.5Z"
+        clipRule="evenodd"
+      />
+      <path
+        fillRule="evenodd"
+        d="M4.72 11.47a.75.75 0 0 0 0 1.06l7.5 7.5a.75.75 0 1 0 1.06-1.06L6.31 12l6.97-6.97a.75.75 0 0 0-1.06-1.06l-7.5 7.5Z"
+        clipRule="evenodd"
+      />
+    </svg>
+  );
+}
