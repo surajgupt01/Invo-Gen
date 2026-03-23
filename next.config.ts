f0154e440e2130d2a04f726390192c7c138a3964
surajgupt01 <surajgupt880@gmail.com>
2026-03-23 19:36:13 +0530

    Free-Tier

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
+// next.config.ts
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  serverExternalPackages: ["@sparticuz/chromium", "puppeteer-core"],
 };
+
+
 
 export default nextConfig;
