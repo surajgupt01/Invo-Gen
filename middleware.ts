da92b081e8cbd388b6ceb389420b04fcda20db23
surajgupt01 <surajgupt880@gmail.com>
2026-03-27 23:08:58 +0530

    middle ware changes

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,20 @@
-import { auth } from "@/auth"
+// middleware.ts
+import { getToken } from "next-auth/jwt"
 import { NextResponse } from "next/server"
+import type { NextRequest } from "next/server"
 
-export default auth((req) => {
-  if (!req.auth && req.nextUrl.pathname.startsWith("/dashboard")) {
+export async function middleware(req: NextRequest) {
+  const token = await getToken({
+    req,
+    secret: process.env.AUTH_SECRET,
+  })
+
+  if (!token && req.nextUrl.pathname.startsWith("/dashboard")) {
     return NextResponse.redirect(new URL("/signin", req.url))
   }
-})
+
+  return NextResponse.next()
+}
 
 export const config = {
   matcher: ["/dashboard/:path*"],
